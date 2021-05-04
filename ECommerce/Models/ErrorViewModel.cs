ea83070382590224a3204dfa2131ed1d4d57cc5f
LongNguyendh <longdhn.lk@gmail.com>
2021-05-04 17:42:11 +0700

    Header layout

---
+++ ECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
